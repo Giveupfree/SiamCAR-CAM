47af7b4dd14e720a0c4e895682556067351e687b
Huiyiqianli <32036062+Giveupfree@users.noreply.github.com>
2022-07-24 18:05:50 +0800

    Update __init__.py

--- CAM/__init__.py
+++ CAM/__init__.py
@@ -7,19 +7,13 @@ from __future__ import unicode_literals
 
 from CAM.GradCAM import GradCAM, GradCAMpp, SmoothGradCAM
 from CAM.GroupCAM import GroupCAM
-from CAM.Guided_Backprop import GuidedBackProp
-from CAM.Integrated_Gradidents import IntegratedGradients
 from CAM.ScoreCAM import ScoreCAM
-from CAM.Smooth_Integrated import SmoothIntGrad
 
 CAM = {
     'GradCAM': GradCAM,
     'GradCAMpp': GradCAMpp,
     "SmoothGradCAM": SmoothGradCAM,
-    "GuidedBackProp": GuidedBackProp, # 不能运行
-    "IntegratedGradients": IntegratedGradients, # 不能运行
     'ScoreCAM': ScoreCAM,
-    'SmoothIntGrad': SmoothIntGrad,
     'GroupCAM': GroupCAM,
 }
 
